1f8a12598d6e559c76cb239efc24dfecaeac6ad8
Buddhadev Tiwari <budhdev@gnxtsystems.com>
2024-12-19 11:30:14 +0530

    estimate list js modified

--- MrGutter.Web/wwwroot/theme_assets/js/app-Estimate-list.js
+++ MrGutter.Web/wwwroot/theme_assets/js/app-Estimate-list.js
@@ -80,20 +80,7 @@ $(function () {
                     responsivePriority: 4,
                     render: function (data, type, full, meta) {
                         var $name = full['est_name'];
-                        if ($image) {
-                            // For Avatar image
-                            var $output =
-                                '<img src="' + assetsPath + 'img/avatars/' + $image + '" alt="Avatar" class="rounded-circle">';
-                        } else {
-                            // For Avatar badge
-                            var stateNum = Math.floor(Math.random() * 6);
-                            var states = ['success', 'danger', 'warning', 'info', 'primary', 'secondary'];
-                            var $state = states[stateNum],
-                                $name = full['est_name'],
-                                $initials = $name.match(/\b\w/g) || [];
-                            $initials = (($initials.shift() || '') + ($initials.pop() || '')).toUpperCase();
-                            $output = '<span class="avatar-initial rounded-circle bg-label-' + $state + '">' + $initials + '</span>';
-                        }
+                        
                         // Creates full output for row
                         var $row_output =
                             '<div class="d-flex justify-content-start align-items-center user-name">' +
@@ -130,30 +117,7 @@ $(function () {
                 },
                 
              
-                //{
-                //  // Plans
-                //  targets: 4,
-                //  render: function (data, type, full, meta) {
-                //    var $plan = full['current_plan'];
-
-                //    return '<span class="text-heading">' + $plan + '</span>';
-                //  }
-                //},
-                //{
-                //    // User Status
-                //    targets: 5,
-                //    render: function (data, type, full, meta) {
-                //        var $status = full['status'];
-
-                //        return (
-                //            '<span class="badge ' +
-                //            statusObj[$status].class +
-                //            '" text-capitalized>' +
-                //            statusObj[$status].title +
-                //            '</span>'
-                //        );
-                //    }
-                //},
+                
                 {
                     // address
                     targets: 4,
@@ -166,18 +130,19 @@ $(function () {
                         );
                     }
                 },
-                //{
-                //    // status
-                //    targets: 5,
-                //    render: function (data, type, full, meta) {
-                //        var $email = full['status'];
-                //        return (
-                //            "<span class='text-truncate d-flex align-items-center text-heading'>" +
-                //            $email +
-                //            '</span>'
-                //        );
-                //    }
-                //},
+                {
+                    // status
+                    targets: 5,
+                    render: function (data, type, full, meta) {
+                        var $email = full['address'];
+                        return (
+                            "<span class='text-truncate d-flex align-items-center text-heading'>" +
+                            $email +
+                            '</span>'
+                        );
+                    }
+                },
+               
                 {
                     // Actions
                     targets: -1,
@@ -192,11 +157,6 @@ $(function () {
                             '" class="btn btn-icon btn-text-secondary waves-effect waves-light rounded-pill"><i class="ti ti-eye ti-md"></i></a>' +
                             '<a href="javascript:;" class="btn btn-icon btn-text-primary waves-effect waves-light rounded-pill"><i class="ti ti-edit ti-md me-2"></i></a>' +
                             '<a href="javascript:;" class="btn btn-icon btn-text-danger waves-effect waves-light rounded-pill delete-record"><i class="ti ti-trash ti-md"></i></a>' +
-                            //'<a href="javascript:;" class="btn btn-icon btn-text-secondary waves-effect waves-light rounded-pill dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical ti-md"></i></a>' +
-                            //'<div class="dropdown-menu dropdown-menu-end m-0">' +
-                            //'<a href="javascript:;"" class="dropdown-item">Edit</a>' +
-                            //'<a href="javascript:;" class="dropdown-item">Suspend</a>' +
-                            //'</div>' +
                             '</div>'
                         );
                     }
@@ -207,8 +167,7 @@ $(function () {
             dom:
                 '<"row"' +
                 '<"col-md-2"<"ms-n2"l>>' +
-               // '<"col-md-10"<"dt-action-buttons text-xl-end text-lg-start text-md-end text-start d-flex align-items-center justify-content-end flex-md-row flex-column mb-6 mb-md-0 mt-n6 mt-md-0"fB>>' +
-                '<"col-md-10"<"dt-action-buttons text-xl-end text-lg-start text-md-end text-start d-flex align-items-center justify-content-end flex-md-row flex-column mb-6 mb-md-0 mt-n6 mt-md-0"fB>>' +
+                '<"col-md-10"<"dt-action-buttons text-xl-end text-lg-start text-md-end text-start d-flex align-items-center justify-content-end flex-md-row flex-column mb-6 mb-md-0 mt-n6 mt-md-0"f>>' +
                 '>t' +
                 '<"row"' +
                 '<"col-sm-12 col-md-6"i>' +
@@ -224,264 +183,10 @@ $(function () {
                 }
             },
             // Buttons with Dropdown
-            buttons: [
-            //  {
-            //    extend: 'collection',
-            //    className: 'btn btn-label-secondary dropdown-toggle mx-4 waves-effect waves-light',
-            //    text: '<i class="ti ti-upload me-2 ti-xs"></i>Export',
-               // buttons: [
-                  //{
-                  //  extend: 'print',
-                  //  text: '<i class="ti ti-printer me-2" ></i>Print',
-                  //  className: 'dropdown-item',
-                  //  exportOptions: {
-                  //    columns: [1, 2, 3, 4, 5],
-                  //    // prevent avatar to be print
-                  //    format: {
-                  //      body: function (inner, coldex, rowdex) {
-                  //        if (inner.length <= 0) return inner;
-                  //        var el = $.parseHTML(inner);
-                  //        var result = '';
-                  //        $.each(el, function (index, item) {
-                  //          if (item.classList !== undefined && item.classList.contains('user-name')) {
-                  //            result = result + item.lastChild.firstChild.textContent;
-                  //          } else if (item.innerText === undefined) {
-                  //            result = result + item.textContent;
-                  //          } else result = result + item.innerText;
-                  //        });
-                  //        return result;
-                  //      }
-                  //    }
-                  //  },
-                  //  customize: function (win) {
-                  //    //customize print view for dark
-                  //    $(win.document.body)
-                  //      .css('color', headingColor)
-                  //      .css('border-color', borderColor)
-                  //      .css('background-color', bodyBg);
-                  //    $(win.document.body)
-                  //      .find('table')
-                  //      .addClass('compact')
-                  //      .css('color', 'inherit')
-                  //      .css('border-color', 'inherit')
-                  //      .css('background-color', 'inherit');
-                  //  }
-                  //},
-                  //{
-                  //  extend: 'csv',
-                  //  text: '<i class="ti ti-file-text me-2" ></i>Csv',
-                  //  className: 'dropdown-item',
-                  //  exportOptions: {
-                  //    columns: [1, 2, 3, 4, 5],
-                  //    // prevent avatar to be display
-                  //    format: {
-                  //      body: function (inner, coldex, rowdex) {
-                  //        if (inner.length <= 0) return inner;
-                  //        var el = $.parseHTML(inner);
-                  //        var result = '';
-                  //        $.each(el, function (index, item) {
-                  //          if (item.classList !== undefined && item.classList.contains('user-name')) {
-                  //            result = result + item.lastChild.firstChild.textContent;
-                  //          } else if (item.innerText === undefined) {
-                  //            result = result + item.textContent;
-                  //          } else result = result + item.innerText;
-                  //        });
-                  //        return result;
-                  //      }
-                  //    }
-                  //  }
-                  //},
-                  //{
-                  //  extend: 'excel',
-                  //  text: '<i class="ti ti-file-spreadsheet me-2"></i>Excel',
-                  //  className: 'dropdown-item',
-                  //  exportOptions: {
-                  //    columns: [1, 2, 3, 4, 5],
-                  //    // prevent avatar to be display
-                  //    format: {
-                  //      body: function (inner, coldex, rowdex) {
-                  //        if (inner.length <= 0) return inner;
-                  //        var el = $.parseHTML(inner);
-                  //        var result = '';
-                  //        $.each(el, function (index, item) {
-                  //          if (item.classList !== undefined && item.classList.contains('user-name')) {
-                  //            result = result + item.lastChild.firstChild.textContent;
-                  //          } else if (item.innerText === undefined) {
-                  //            result = result + item.textContent;
-                  //          } else result = result + item.innerText;
-                  //        });
-                  //        return result;
-                  //      }
-                  //    }
-                  //  }
-                  //},
-                  //{
-                  //  extend: 'pdf',
-                  //  text: '<i class="ti ti-file-code-2 me-2"></i>Pdf',
-                  //  className: 'dropdown-item',
-                  //  exportOptions: {
-                  //    columns: [1, 2, 3, 4, 5],
-                  //    // prevent avatar to be display
-                  //    format: {
-                  //      body: function (inner, coldex, rowdex) {
-                  //        if (inner.length <= 0) return inner;
-                  //        var el = $.parseHTML(inner);
-                  //        var result = '';
-                  //        $.each(el, function (index, item) {
-                  //          if (item.classList !== undefined && item.classList.contains('user-name')) {
-                  //            result = result + item.lastChild.firstChild.textContent;
-                  //          } else if (item.innerText === undefined) {
-                  //            result = result + item.textContent;
-                  //          } else result = result + item.innerText;
-                  //        });
-                  //        return result;
-                  //      }
-                  //    }
-                  //  }
-                  //},
-                  //{
-                  //  extend: 'copy',
-                  //  text: '<i class="ti ti-copy me-2" ></i>Copy',
-                  //  className: 'dropdown-item',
-                  //  exportOptions: {
-                  //    columns: [1, 2, 3, 4, 5],
-                  //    // prevent avatar to be display
-                  //    format: {
-                  //      body: function (inner, coldex, rowdex) {
-                  //        if (inner.length <= 0) return inner;
-                  //        var el = $.parseHTML(inner);
-                  //        var result = '';
-                  //        $.each(el, function (index, item) {
-                  //          if (item.classList !== undefined && item.classList.contains('user-name')) {
-                  //            result = result + item.lastChild.firstChild.textContent;
-                  //          } else if (item.innerText === undefined) {
-                  //            result = result + item.textContent;
-                  //          } else result = result + item.innerText;
-                  //        });
-                  //        return result;
-                  //      }
-                  //    }
-                  //  }
-                  //}
-               // ]
-             // },
-             
-                {
-                    text: '<i class="ti ti-plus me-0 me-sm-1 ti-xs"></i><span class="d-none d-sm-inline-block">Add New Estimate</span>',
-                    className: 'add-new btn btn-primary waves-effect waves-light mx-4',
-                    attr: {
-                        'data-bs-toggle': 'offcanvas',
-                        'data-bs-target': '#offcanvasAddCompany'
-                    }
-                }
-            ],
+       
             // For responsive popup
-            responsive: {
-                details: {
-                    display: $.fn.dataTable.Responsive.display.modal({
-                        header: function (row) {
-                            var data = row.data();
-                            return 'Details of ' + data['_name'];
-                        }
-                    }),
-                    type: 'column',
-                    renderer: function (api, rowIdx, columns) {
-                        var data = $.map(columns, function (col, i) {
-                            return col.title !== '' // ? Do not show row in modal popup if title is blank (for check box)
-                                ? '<tr data-dt-row="' +
-                                col.rowIndex +
-                                '" data-dt-column="' +
-                                col.columnIndex +
-                                '">' +
-                                '<td>' +
-                                col.title +
-                                ':' +
-                                '</td> ' +
-                                '<td>' +
-                                col.data +
-                                '</td>' +
-                                '</tr>'
-                                : '';
-                        }).join('');
-
-                        return data ? $('<table class="table"/><tbody />').append(data) : false;
-                    }
-                }
-            },
-            initComplete: function () {
-                // Adding role filter once table initialized
-                this.api()
-                    .columns(3)
-                    .every(function () {
-                        var column = this;
-                        var select = $(
-                            '<select id="UserRole" class="form-select text-capitalize"><option value=""> Select Role </option></select>'
-                        )
-                            .appendTo('.user_role')
-                            .on('change', function () {
-                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
-                                column.search(val ? '^' + val + '$' : '', true, false).draw();
-                            });
-
-                        column
-                            .data()
-                            .unique()
-                            .sort()
-                            .each(function (d, j) {
-                                select.append('<option value="' + d + '">' + d + '</option>');
-                            });
-                    });
-                // Adding plan filter once table initialized
-                this.api()
-                    .columns(4)
-                    .every(function () {
-                        var column = this;
-                        var select = $(
-                            '<select id="UserPlan" class="form-select text-capitalize"><option value=""> Select Plan </option></select>'
-                        )
-                            .appendTo('.user_plan')
-                            .on('change', function () {
-                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
-                                column.search(val ? '^' + val + '$' : '', true, false).draw();
-                            });
-
-                        column
-                            .data()
-                            .unique()
-                            .sort()
-                            .each(function (d, j) {
-                                select.append('<option value="' + d + '">' + d + '</option>');
-                            });
-                    });
-                // Adding status filter once table initialized
-                this.api()
-                    .columns(6)
-                    .every(function () {
-                        var column = this;
-                        var select = $(
-                            '<select id="FilterTransaction" class="form-select text-capitalize"><option value=""> Select Status </option></select>'
-                        )
-                            .appendTo('.user_status')
-                            .on('change', function () {
-                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
-                                column.search(val ? '^' + val + '$' : '', true, false).draw();
-                            });
-
-                        column
-                            .data()
-                            .unique()
-                            .sort()
-                            .each(function (d, j) {
-                                select.append(
-                                    '<option value="' +
-                                    statusObj[d].title +
-                                    '" class="text-capitalize">' +
-                                    statusObj[d].title +
-                                    '</option>'
-                                );
-                            });
-                    });
-            }
+          
+           
         });
     }
 
