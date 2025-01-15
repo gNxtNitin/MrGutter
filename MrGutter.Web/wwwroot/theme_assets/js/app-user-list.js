9869bcb1b4aa421173d03fe0b4ba4a4d462de422
Parth Sharma <Parth Sharma@DESKTOP-7G3KUA7>
2025-01-15 18:38:52 +0530

    Updated cmp and user

--- MrGutter.Web/wwwroot/theme_assets/js/app-user-list.js
+++ MrGutter.Web/wwwroot/theme_assets/js/app-user-list.js
@@ -26,29 +26,27 @@ $(document).ready(function () {
                 "data": null,
                 "render": function (data, type, row) {
                     return `
-                      <a class="text-danger" style="cursor: pointer;" onclick="openEditModal1(${row.userID})">
+                      <a class="text-danger" style="cursor: pointer;" title="Edit User" onclick="openEditModal1(${row.userID})">
                            <i class='fa-solid fa-pen-to-square text-danger'></i>
                       </a>
-                       <a href="#" class="text-danger" style="cursor: pointer;" onclick="deleteUserRow(${row.userID})">
+                       <a href="#" class="text-danger" style="cursor: pointer;" title="Delete User" onclick="deleteUserRow(${row.userID})">
                            <i class='ti ti-trash text-danger'></i>
                       </a>
-                          <a href="#" class="text-danger" style="cursor: pointer;">
-                           <i class='fa-regular fa-clipboard text-danger'></i>
-                      </a>
+                  
 
                     `;
                 }
             },
-            {
-                "data": "",
-                "name": "checkbox",
-                "autoWidth": true,
-                "render": function (data, type, row) {
-                    return `<input type="checkbox" class="select-row" data-user-id="${row.userID}" />`;
-                },
-                "orderable": false,
-                "searchable": false
-            },
+            //{
+            //    "data": "",
+            //    "name": "checkbox",
+            //    "autoWidth": true,
+            //    "render": function (data, type, row) {
+            //        return `<input type="checkbox" class="select-row" data-user-id="${row.userID}" />`;
+            //    },
+            //    "orderable": false,
+            //    "searchable": false
+            //},
            
         ],
                buttons: [
@@ -109,33 +107,71 @@ const selectedUsers = new Set();
 
 
 //Row deletion
-function deleteUserRow(userID) {
-    // Confirm the deletion with the user
-    if (confirm("Are you sure you want to delete this user?")) {
-        // Perform the AJAX request to delete the user
-        $.ajax({
-            url: '/userManager/DeleteUser',  
-            method: 'POST',
-            data: { userID: userID },  
-            success: function (response) {
-                if (response.success) {
-                    // User deleted successfully, reload the DataTable
-                    $('#userDatatable').DataTable().ajax.reload();
-                    alert("User deleted successfully.");
-                    location.reload();
+//function deleteUserRow(userID) {
+//    // Confirm the deletion with the user
+//    if (confirm("Are you sure you want to delete this user?")) {
+//        // Perform the AJAX request to delete the user
+//        $.ajax({
+//            url: '/userManager/DeleteUser',
+//            method: 'POST',
+//            data: { userID: userID },
+//            success: function (response) {
+//                if (response.success) {
+//                    // User deleted successfully, reload the DataTable
+//                    $('#userDatatable').DataTable().ajax.reload();
+//                    alert("User deleted successfully.");
+//                    location.reload();
 
-                } else {
-                    // If there was an issue deleting the user
-                    alert("User deleted successfully.");
-                    location.reload();                }
-            },
-            error: function (xhr, status, error) {
-                // If there was an error with the AJAX request
-                alert("User deleted successfully.");
-                location.reload();            }
-        });
-    }
+//                } else {
+//                    // If there was an issue deleting the user
+//                    alert("User deleted successfully.");
+//                    location.reload();                }
+//            },
+//            error: function (xhr, status, error) {
+//                // If there was an error with the AJAX request
+//                alert("User deleted successfully.");
+//                location.reload();            }
+//        });
+//    }
+//}
+
+function deleteUserRow(userID) {
+    $('#delete').modal('show');
+
+    $('#delete .confirm-delete').data('user-id', userID);
 }
+
+$('#delete .confirm-delete').on('click', function () {
+    var userID = $(this).data('user-id'); 
+
+    $.ajax({
+        url: '/userManager/DeleteUser', 
+        method: 'POST',
+        data: { userID: userID },  
+        success: function (response) {
+            console.log("Response from server:", response); 
+
+            if (response.success) {
+                
+                $('#delete').modal('hide');
+                $('#userDatatable').DataTable().ajax.reload();
+                alert("User deleted successfully.");
+            } else {
+                $('#delete').modal('hide');
+                $('#userDatatable').DataTable().ajax.reload();
+                
+            }
+        },
+        error: function (xhr, status, error) {
+            // Log the error for debugging
+            //console.error("Error with AJAX request:", error);
+            //alert("Error: Unable to delete the user.");
+            $('#delete').modal('hide');
+            $('#userDatatable').DataTable().ajax.reload();
+           
+        }
+    });
+});
 
 
 function openEditModal1(userID) {
@@ -175,6 +211,47 @@ function openEditModal1(userID) {
     });
 }
 
+
+function UserViewEdit(userID) {
+    alert("inside UserView")
+    debugger
+
+    $.ajax({
+ 
+        url: '/UserManager/UserView?userID=' + userID,
+        method: 'GET',
+        success: function (response) {
+            alert("response : " + response);
+            // Populate other modal fields
+            $('#userView #editUserID').val(response.userID);
+            $('#userView #fName').val(response.firstName);
+            $('#userView #lName').val(response.lastName);
+            $('#userView #editEmail').val(response.email);
+            $('#userView #editPhone').val(response.mobile);
+            $('#userView #editStatus').val(response.userStatus);
+            $('#userView #editIsActive').val(response.isActive);
+
+            // Populate roles dropdown
+            const roleDropdown = $('#userView #editRole');
+            roleDropdown.empty();
+
+            if (response.roles && response.roles.length > 0) {
+                response.roles.forEach(role => {
+                    const isSelected = role.roleID.toString() === response.roleID.toString();
+                    const option = `<option value="${role.roleID}" ${isSelected ? 'selected' : ''}>${role.roleName}</option>`;
+                    roleDropdown.append(option);
+                });
+            } else {
+                roleDropdown.append('<option value="">No roles available</option>');
+            }
+
+            $('#userView').modal('show');
+        },
+        error: function (xhr, status, error) {
+            alert("Error: " + error);
+        }
+    });
+}
 
 
 function saveChanges1() {
